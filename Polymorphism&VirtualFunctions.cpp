848e610710376c8d4a7c93d185d14b74e032aa76
Aditya Raj <aditya1.raj@aot.edu.in>
2022-05-28 18:39:55 +0530

    do it

---
+++ Polymorphism&VirtualFunctions.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Person{
+public:
+	virtual void introduce(){						//virtual functions will not work if it has been overridden in subclass.
+		cout<<"Hi from Person."<<endl;
+	}
+};
+
+class Student : public Person{
+public:
+	void introduce(){
+		cout<<"Hi from Student."<<endl;
+	}
+};
+
+class Farmer : public Person{
+public:
+	void introduce(){
+		cout<<"Hi from Farmer."<<endl;
+	}
+};
+void whosThis(Person &p){
+	p.introduce();
+}
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    Student s;
+    whosThis(s);
+
+    Farmer f;
+    whosThis(f);
+
+    Person *p = new Student();
+    p->introduce();
+}
